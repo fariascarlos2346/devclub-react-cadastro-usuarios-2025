3714857f1a618355d4cbbb0e99067575b3b16103
Carlos <fariascarlos2346@gmail.com>
2025-03-18 22:33:46 -0300

    Estilos Globais no React

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
+import { Title, Container } from './styles'
 
 function App() {
 
-  const myText = 'Amor Amor'
   return (
-      <div>
-      <h1>{myText}</h1>
-      </div>        
+    <Container>
+      <Title>Ok My dear</Title>
+    </Container>
   )
 }
 
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    //background-color: black;
+`
+
+export const Title = styled.h2`
+    color: blue;
+    font-size: 30px;
+   
+
+`
