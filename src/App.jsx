64404c5456f1c9d8eeee0f1e4ed6432b735c0631
Carlos <fariascarlos2346@gmail.com>
2025-03-19 07:36:30 -0300

    começando a estilização com CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { Title, Container } from './styles'
+import { Title, Container, TopBackground, Form, ContainerInputs, Input, Button } from './styles'
 
-function App() {
+function Home() {
 
   return (
     <Container>
-      <Title>Ok My dear</Title>
+      <TopBackground>
+        <img />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastro Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>Nome<span>*</span></InputLabel>
+              <Input type='text' placeholder='Nome do usuário' />
+              <Input type='number' placeholder='Idade do usuário' />
+            </div>
+          </div>
+
+          <div>
+            <InputLabel>
+              E-mail<span>*</span>
+            </InputLabel>
+            <Input type='email' placeholder='E-mail do usuário' />
+          </div>         
+        </ContainerInputs>
+        <Button>Cadastro Usuário</Button>
+      </Form>
     </Container>
   )
 }
--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,19 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    //background-color: black;
-`
+export const Container = styled.div``
+
+export const TopBackground = styled.div``
+
+export const Form = styled.form``
+
+export const ContainerInputs = styled.div``
+
+export const Input = styled.input``
+
+export const InputLabel = styled.label``
+
+export const Button = styled.button``
 
 export const Title = styled.h2`
-    color: blue;
-    font-size: 30px;
    
-
 `
